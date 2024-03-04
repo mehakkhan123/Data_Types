deaacb52ae96f5bec7ef2a866a86e971626f6d6c
mehakkhan123 <mahekkhan284@gmail.com>
2024-03-04 12:32:11 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Data types
+// String
+let myName = "Mehak Khan";
+console.log(myName);
+// Number
+let favNumber = 8;
+console.log(favNumber);
+// Boolean
+let isRaining = false;
+console.log(isRaining);
+// Undefined
+let undefinedValue = undefined;
+console.log(undefinedValue);
+// Null
+let nullValue = null;
+console.log(nullValue);
+// Symbol
+let uniqueSymbol = Symbol("Unique");
+console.log(uniqueSymbol);
+// BigInt
+let bigNumber = 200000n;
+console.log(bigNumber);
+// Any
+let anyValue = "What's Up";
+console.log(anyValue);
+export {};
---
+++ app.ts
@@ -0,0 +1,25 @@
+// Data types
+// String
+let myName:String = "Mehak Khan";
+console.log(myName);
+// Number
+let favNumber:number = 8;
+console.log(favNumber);
+// Boolean
+let isRaining:Boolean = false;
+console.log(isRaining);
+// Undefined
+let undefinedValue:undefined = undefined;
+console.log(undefinedValue);
+// Null
+let nullValue:null = null;
+console.log(nullValue);
+// Symbol
+let uniqueSymbol:symbol = Symbol("Unique");
+console.log(uniqueSymbol);
+// BigInt
+let bigNumber:bigint = 200000n;
+console.log(bigNumber);
+// Any
+let anyValue:any = "What's Up";
+console.log(anyValue);
